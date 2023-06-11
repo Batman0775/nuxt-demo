31e8ba9e263949e1d60008e7521ac2b6b356786f
Batman0775 <batmanthe775@gmail.com>
2023-06-11 03:27:32 -0700

    okkkkk

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,42 @@
 <template>
     <div>
-        <h1>This is home page</h1>
+        <section class="hero py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div>
+                        <h1>This is home Page.</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam voluptate ipsam exercitationem eius, cum praesentium quo blanditiis illo vero officia aspernatur quis eos non culpa temporibus corporis atque? Commodi facere ipsam totam reprehenderit? Explicabo optio porro numquam provident sunt dolorem expedita cumque officiis, eveniet, voluptas natus voluptatibus, soluta fugit minus?</p>
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
+    
+    <!-- card -->
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                       <div>
+                        <img src="https://cdn.pixabay.com/photo/2023/03/02/20/59/town-hall-tower-7826491__340.jpg" alt="">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, aut deserunt. Suscipit dolore repellat maxime, similique vero iure? Asperiores, beatae, temporibus vero minus nemo iste id corrupti aliquid voluptatibus corporis amet sunt eligendi, neque tempore molestiae ducimus dolorum veniam ab quasi! Quod tempore quaerat aperiam iure fugit molestias deleniti incidunt!</p>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup>
 
 </script>
 
-<style scoped>
-
+<style  scoped>
+.hero{
+    background-color: gray
+}
 </style>
