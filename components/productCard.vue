780e87a6d757dcde862590f77bacd6adee825ea8
Batman0775 <batmanthe775@gmail.com>
2023-06-18 03:26:07 -0700

    spooky

--- components/productCard.vue
+++ components/productCard.vue
@@ -1,20 +1,24 @@
 <template>
 <div>
     <div class="card" style="width: 18rem;">
-        <div class="img-box">
-            <img src="https://cdn.pixabay.com/photo/2019/05/31/01/53/chowmein-4241265_640.jpg" class="card-img-top" alt="...">
+        <div class="image-box">
+            <img :src="product.images[0]" class="card-img-top" alt="...">
         </div>
         <div class="card-body">
-            <h5 class="card-title">Chowmein</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus deserunt laudantium vero sapiente rerum quos, alias enim ratione nam itaque est, fugit reprehenderit distinctio harum quisquam sunt culpa a aliquam!</p>
-            <a href="https://www.youtube.com/" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">{{ product.title }}</h5>
+            <p class="card-text">{{ product.description }}</p>
+            <p class="card-text">{{ product.price}}</p>
+            <p class="card-text">{{ product.updatedAt }}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
 </div>
 </template>
 
 <script setup>
-
+const props = defineProps({
+    product:{}
+})
 </script>
 
 <style  scoped>
